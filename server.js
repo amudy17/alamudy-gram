d864a184998b4d81e2cef4d2af1a81554bc60cb8
amudy17 <syarif.amudi@gmail.com>
2018-08-07 14:13:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ io.on('connection', function(client){
 
 function sendTelegramMessage(chatId, text, parseMode) {
     request
-        .post('https://api.telegram.org/bot' + process.env.566292794:AAHy0mNe-pNncAJuE5jX_UHGENkHFweAIgw + '/sendMessage')
+        .post('https://api.telegram.org/bot' + process.env.TELEGRAM_TOKEN + '/sendMessage')
         .form({
             "chat_id": chatId,
             "text": text,
